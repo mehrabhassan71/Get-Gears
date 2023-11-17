7975b2bd9c9ea407130d2d982961252b6a449724
mehrab <mehrabhassan71@gmail.com>
2023-11-17 22:36:29 +0600

    final_build

--- src/oppData.js
+++ src/oppData.js
@@ -1,25 +1,25 @@
 console.log(`connet`);
 const iphoneContainer = document.getElementById(`iphone-container`);
-const getOppoData = ()=>{
-    fetch('https://openapi.programming-hero.com/api/phones?search=oppo')
+const getOppoData = () => {
+  fetch('https://openapi.programming-hero.com/api/phones?search=oppo')
     .then(res => res.json())
     .then(json => oppoDisplay(json))
-    
+
 }
 
-let oppoDisplay = (oppo) =>{
-    console.log(oppo);
-    oppo.data.forEach(oppoPhone =>{
-        console.log(oppoPhone);
-        const div = document.createElement(`div`);
-        
-         div.innerHTML = `
-        <div  class="w-fit mx-auto"> 
+let oppoDisplay = (oppo) => {
+  console.log(oppo);
+  oppo.data.forEach(oppoPhone => {
+    console.log(oppoPhone);
+    const div = document.createElement(`div`);
+
+    div.innerHTML = `
+         <div  class="w-fit mx-auto"> 
             <div class="card w-72 bg-base-100 shadow-xl">
                 <figure><img class="p-3" src="${oppoPhone.image}" alt="Shoes" /></figure>
                 <div class="card-body">
                   <h2 class="card-title">${oppoPhone.phone_name
-                  }</h2>
+      }</h2>
                   <p> ${oppoPhone.brand}  does he choose?</p>
                   <p>${oppoPhone.slug}</p>
                   <div class="card-actions justify-end">
@@ -29,9 +29,9 @@ let oppoDisplay = (oppo) =>{
               </div>
         </div>
         `
-        iphoneContainer.appendChild(div)
-    })
-} 
+    iphoneContainer.appendChild(div)
+  })
+}
 
 
 getOppoData();
